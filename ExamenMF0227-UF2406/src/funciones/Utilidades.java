55674b53aead08dc04037a77c65d8518e05b92a2
David <David@DESKTOP-1VREN8U>
2022-08-18 09:47:09 +0200

    Commit incial antes del examen

---
+++ ExamenMF0227-UF2406/src/funciones/Utilidades.java
@@ -0,0 +1,25 @@
+package funciones;
+
+public class Utilidades {
+
+	
+
+	public String devuelveNota(double nota) {
+		String descripcion = "Matrícula";
+		
+		if (nota<0.0 || nota >10) {
+			throw new ArithmeticException("La nota debe estar entre 0.0 y 10");
+		} else if (nota<5.0) {
+			descripcion="Suspenso";
+		} else if (nota <6.5) {
+			descripcion = "Bien";
+		} else if (nota<8.5) {
+			descripcion ="Notable";
+		} else if (nota<10) {
+			descripcion ="Sobresaliente";
+		} else {
+			descripcion = "Matrícula";
+		}
+		return descripcion;
+	}
+}
